c9fe269ca8eefa91679f7a5dd42b1e6082df8126
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-03-13 05:02:13 -0400

    added oauth file

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+	twitter: {
+		consumerKey: process.env.TWITTER_clientID,
+		consumerSecret: process.env.TWITTER_clientSecret,
+		callbackURL: process.env.TWITTER_callbackURL
+	}
+}
+
+module.exports = ids
